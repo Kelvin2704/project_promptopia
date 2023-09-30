871c4670f059d84be3de3ea1df44664b4318a445
Kelvin2704 <locnguyenphuocloc67@gmail.com>
2023-09-30 18:46:37 +0700

    'first-commit'

---
+++ utils/database.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+let isConnected = false;
+
+export const connectToDB = async () => {
+  mongoose.set("strictQuery", true);
+  if (isConnected) {
+    console.log("Mongodb is already connected");
+    return;
+  }
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "share-prompt",
+    });
+    isConnected = true;
+  } catch (error) {
+    console.log("database error:", error);
+  }
+};
